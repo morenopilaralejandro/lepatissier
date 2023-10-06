999796e759116f6120af6138e97df41f926c5e4e
alejandro <morenopilaralejandro@gmail.com>
2023-10-06 22:44:20 +0200

    alle insert delete

--- view/test.php
+++ view/test.php
@@ -1,6 +1,18 @@
 <?php
-require_once __DIR__ . '/../php/DbConnection.php';
-$con = new DbConnection();
-$link = $con->getConnection();
+require_once __DIR__ . '/../php/class/Alle.php';
+$alleObj = Alle::factory();
+//$alleObj->insert(0, "asd", "asdasd");
+$alleResult = $alleObj->getAlleByProdId(3);
+
+while($row = $alleResult->fetch_assoc()) {
+    $alleObj = new Alle(
+        $row['alleId'], 
+        $row['alleNameEn'],
+        $row['alleNameEs']);
+    print_r($alleObj);
+    //print_r($alleObj);
+    //print_r($alleObj->getAlleId());
+    echo "<br>";
+}
 echo "test";
 ?>
